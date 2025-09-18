ee18a549a6fa1c9b350c7c23be6f0620ee155ed3
Lluis Franco <hello@lluisfranco.com>
2025-09-18 18:29:48 +0200

    Recent files list user control

---
+++ TestSpreadsheet/RecentFilesListUserControl.Designer.cs
@@ -0,0 +1,44 @@
+﻿namespace TestSpreadsheet
+{
+    partial class RecentFilesListUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // RecentFilesListUserControl
+            // 
+            AutoScaleDimensions = new SizeF(6F, 13F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Name = "RecentFilesListUserControl";
+            Size = new Size(1002, 623);
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
